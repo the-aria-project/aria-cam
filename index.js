63769fb4861b8be42ecd01695eb1e4690e0b0a81
bcarr610 <carrbrandon4@gmail.com>
2023-01-09 11:46:35 -0500

    Added config and socket.io-client to emit events to external wss servers

--- index.js
+++ index.js
@@ -1,48 +1,91 @@
+const os = require('os')
 const http = require('http')
-const fs = require('fs')
 const path = require('path')
-const spawn = require('child_process').spawn
 const socketIO = require('socket.io')
+const socketIOClient = require('socket.io-client')
 const express = require('express')
+const RaspividJpegStream = require('raspivid-jpeg-stream')
 
 const app = express()
 const server = http.Server(app)
 const io = socketIO(server)
-const VideoFeed = require('./VideoFeed')
+const config = require('./config.json')
 
-// State
-const video = new VideoFeed()
-let clientCount = 0
+let clients = []
+let servers = []
 
-const frameHandler = (imagePath, imgUrl, socket) => {
-  socket.emit('frame', imgUrl)
+const camera = new RaspividJpegStream({
+  width: config.camera.width,
+  height: config.camera.height,
+  timeout: 0,
+  framerate: config.camera.framerate
+})
+
+const frameHandler = frame => {
+  const mimeType = 'image/jpg'
+  const b64Buffer = Buffer.from(frame).toString('base64')
+
+  // Emit event to clients
+  clients.forEach(socket => {
+    if (socket) {
+      socket.emit('camera:frame', { mimeType, buffer: b64Buffer })
+    }
+  })
+
+  // Emit event to servers
+  servers.forEach(socket => {
+    if (socket) {
+      socket.emit('camera:frame', { mimeType, buffer: b64Buffer })
+    }
+  })
+  
 }
 
-io.on('connection', socket => {
-  clientCount++
-  console.log('Client connected')
+camera.on('data', frameHandler)
 
-  if (!video.isTicking) {
-    console.log('Recording frames')
-    video.startWritingImages((imgPath, imgUrl) => frameHandler(imgPath, imgUrl, socket))
-  }
+// Clients
+io.on('connection', socket => {
+  console.log(`${socket.id} connected`)
+  clients.push(socket)
+
+  socket.on('disconnect', socket => {
+    console.log(`${socket.id} disconnected`)
+    clients = clients.filter(s => s.id !== socket.id)
+  })
   
 })
 
-io.on('disconnect', () => {
-  clientCount--
-  if (clientCount === 0 && video.isTicking) {
-    console.log('Stopping recording frames')
-    video.stopWritingImages()
-  }
+// Servers
+config.destinations.forEach(dest => {
+  const socket = socketIOClient(`${dest.ssl ? 'https' : 'http'}://${dest.host}:${dest.port}`)
+
+  socket.on('connect', () => {
+    console.log(`[${socket.id}] Connected to ${dest.host}`)
+    servers.push(socket)
+  })
+
+  socket.on('disconnect', () => {
+    console.log(`[${socket.id}] Disconnected from ${dest.host}`)
+    servers = servers.filter(s => s.id !== socket.id)
+  })
+  
 })
 
-app.use(express.static('tmp'))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'))
+app.get(config.server['live_view_path'], (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'live-view.html'))
 })
 
-server.listen(3000, () => {
-  console.log('Server running')
+app.get('/reset', (req, res) => {
+  // Reset the camera
+})
+
+server.listen(config.server.port, () => {
+  console.log(`Camera server running on port ${config.server.port}`)
+  console.log(`View your camera live at http://${os.hostname()}:${[8080, 80].includes(config.server.port) ? config.server.live_view_path : `:${config.server.port}${config.server.live_view_path}`}`)
+
+  console.log(`Camera Config: ${config.camera.width}x${config.camera.height}@${config.camera.framerate}fps`)
+  console.log(`\n__Configured detinations__`)
+  config.destinations.forEach(dest => {
+    console.log(`${dest.ssl ? 'https' : 'http'}://${dest.host}:${dest.port}`)
+  })
 })
--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
 #! bin/bash
 
-sudo apt-get install libjpeg9-dev
-sudo apt-get install imagemagick
-tar xvzf mjpg-streamer-r63.tar.gz
-cd mjpg-streamer-r63
-make
+npm install
