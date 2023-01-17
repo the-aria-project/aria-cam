5f5ae56c529375d651771771fe680edb9ae0da6c
Brandon Carr <carrbrandon4@gmail.com>
2023-01-17 12:01:06 -0500

    Add backend requirement

--- workers/object-prediction-worker.js
+++ workers/object-prediction-worker.js
@@ -1,6 +1,7 @@
 const tf = require('@tensorflow/tfjs-node')
 const { workerData, parentPort } = require('worker_threads')
 const config = require('../config.json')
+require('@tensorflow/tfjs-backend-cpu')
 
 const { model } = workerData
 console.log('Object prediction worker starting')
